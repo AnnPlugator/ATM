0e1201c39acdf6ea69811856fb56f94c76c8c41f
AnnPlugator <shichker1289@gmail.com>
2025-08-02 09:30:51 +0300

    Add framework

--- src/test/java/api/BaseApiTest.java
+++ src/test/java/api/BaseApiTest.java
@@ -1,12 +1,9 @@
 package api;
 
-import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeAll;
 
-public class BaseApiTest {
-
-    @BeforeAll
-    public static void setup() {
-        RestAssured.baseURI = "http://172.17.1.70:32015";
-    }
+public abstract class BaseApiTest {
+    protected final String BASE_URI_INFO_SERVICE = "http://172.17.1.70:32005";
+    protected final String BASE_URI_USER_SERVICE = "http://172.17.1.70:32015";
+    protected final String DEFAULT_CITY = "Москва";
+    protected final String DEFAULT_SEARCH_QUERY = "Арбатская";
 }
