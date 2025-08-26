9b1948ff61570bbb1faf19fe225c00cd0381f065
EleshIK <zinnurt@mail.ru>
2025-08-26 09:30:31 +0300

    TraineTask

---
+++ src/test/java/api/RequestTest.java
@@ -0,0 +1,41 @@
+package api;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+//Zinnur Mubarakshin
+public class RequestTest {
+
+    private Request request = new Request();
+
+    @Test
+    @DisplayName("Проверка GET запроса по городу Санкт-Петербург")
+    public void getRequestCity1() {
+
+        Response response = request.sendingRequest("Санкт-Петербург");
+
+
+        assertEquals(200, response.statusCode(), "Статус кода не 200");
+
+
+        assertTrue(response.jsonPath().getList("suggests").size() > 0, "Массив 'suggests' пустой");
+
+
+        assertEquals("25", response.jsonPath().getString("suggests[0].name"), "Имя отличается от ожидаемого");
+        assertEquals("ул.", response.jsonPath().getString("suggests[0].streetType"), "Тип улицы отличается от ожидаемого");
+        assertEquals("2-я Красноармейская", response.jsonPath().getString("suggests[0].street"), "Улица отличается от ожидаемой");
+        assertEquals("2/27, лит. А", response.jsonPath().getString("suggests[0].house"), "Дом отличается от ожидаемого");
+    }
+
+    @Test
+    @DisplayName("Проверка GET запроса по городу Москва")
+    public void getRequestCity2() {
+
+        Response response = request.sendingRequest("Москва");
+        assertEquals(200, response.statusCode(), "Статус кода не 200");
+        assertTrue(response.jsonPath().getList("suggests").size() > 0, "Массив 'suggests' пустой");
+
+    }
+}
